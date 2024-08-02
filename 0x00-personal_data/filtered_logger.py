e425fb73b3319db5c628ed6558f10358588a9395
Vulcanric <jneric49@gmail.com>
2024-08-02 22:06:21 +0100

    Fixed docstring and code logic

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Defines a function filter_datum that returns log message obfuscated
 """
 import re
+import logging
 from typing import List
 
 
@@ -10,44 +11,57 @@ def filter_datum(
         redaction: str,
         message: str,
         separator: str
-    ) -> str:
+) -> str:
     """ Filters a log message and returns it obfuscated.
 
     Arguments
     _________
 
-        fields <list>:
+        fields : list
             List of strings representing all fields to obfuscate.
 
-        redaction <str>:
+        redaction : str
             String representing by what the field will be obfu-
 scated.
-        message <str>:
+        message : str
             String representing the log line
 
-        separator <str>:
+        separator : str
             String representing by which character is separati-
 ng all fields.
 
     Example
     ________
 
-    >>> fields = ["password", "date_of_birth"]
-    >>> message = "name=matthew;email=mathew@example.com;password=mattpass;date_of_birth=12/3/2005;"
-    >>> filter_datum(fields, 'xxx', message, ';')
-    name=matthew;email=matthew@example.com;password=xxx;date_of_birth=xxx;
+>>> fields = ["password", "dob"]
+>>> message = "name=mark;email=mark@mail.com;password=1234;dob=12/3/2005;"
+>>> filter_datum(fields, 'xxx', message, ';')
+name=matthew;email=matthew@example.com;password=xxx;dob=xxx;
     """
     for field in fields:
-        target_substr, obfuscated = f'{field}=\S+?{separator}', f'{field}={redaction}{separator}'
-        message = re.sub(target_substr, obfuscated, message)
+        message = re.sub(
+                f'{field}=\\S+?{separator}',  # Target field substring
+                f'{field}={redaction}{separator}',  # Obfuscated replacement
+                message
+            )
     return message
 
 
-import logging
-
-
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
+    """ A custom log formatter class, that redacts logs for security.
+    It inherits from the logging.Formatter class
+
+    Attributes
+    __________
+
+    fields : list of str
+        List of strings representing all fields to obfuscate.
+
+    Methods
+    _______
+
+    format(record: logging.LogRecord) -> str:
+        Formats log records, obfuscating PII fields as specified by @fields
     """
 
     REDACTION = "***"
