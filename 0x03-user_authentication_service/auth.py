36c164515b657648ea53a362b01fac5ffa478882
Vulcanric <jneric49@gmail.com>
2024-08-17 05:46:29 +0100

    Implemented a method that returns a user having the given session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,11 +72,8 @@ class Auth:
     def get_user_from_session_id(self, session_id: str) -> Optional[User]:
         """ Finds a user with the given session id
         """
-        if session_id is None:
-            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-        except NoResultFound:
-            user = None
-        finally:
             return user
+        except NoResultFound:
+            return None
