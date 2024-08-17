83a37a27d9ebb22ad1664dae3f9963f0c1d90aa0
Vulcanric <jneric49@gmail.com>
2024-08-17 03:48:14 +0100

    Flask app rendering functionality to web clients

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 """ App module to test simple user authentication application
 """
-from flask import (Flask, jsonify)
+from flask import (Flask, jsonify, request)
+from auth import Auth
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def index():
     """ Respond with a JSON data """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """ Register a new user """
+    user_email = request.form['email']
+    user_password = request.form['password']
+    try:
+        AUTH.register_user(user_email, user_password)
+        response = (jsonify({"email": user_email, "message": "user created"}), 200)
+    except ValueError:  # User is already registered
+        response = (jsonify({"message": "email already registered"}), 400)
+    return response
 
 
 if __name__ == "__main__":
