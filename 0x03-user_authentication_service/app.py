c7b435d9c79ff549375f46c7bd786add02a639a6
Vulcanric <jneric49@gmail.com>
2024-08-17 05:28:30 +0100

    Implemented a login functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ App module to test simple user authentication application
 """
-from flask import (Flask, jsonify, request)
+from flask import (Flask, jsonify, request, make_response, abort)
 from auth import Auth
 
 app = Flask(__name__)
@@ -17,8 +17,8 @@ def index():
 @app.route("/users", methods=["POST"])
 def users():
     """ Register a new user """
-    user_email = request.form['email']
-    user_password = request.form['password']
+    user_email = request.form["email"]
+    user_password = request.form["password"]
     try:
         AUTH.register_user(user_email, user_password)
         response = (
@@ -30,6 +30,22 @@ def users():
                 jsonify({"message": "email already registered"}),
                 400  # Bad request
             )
+    return response
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Verify a user and creates a login session
+    """
+    user_email = request.form["email"]
+    user_password = request.form["password"]
+    if AUTH.valid_login(user_email, user_password):
+        session_id = AUTH.create_session(user_email)
+        payload = {"email": user_email, "message": "logged in"}
+        response = make_response(jsonify(payload))
+        response.set_cookie("Set-Cookie", f"session_id={session_id}")
+    else:
+        abort(401)
     return response
 
 
